5d417241f90a45873ae23cdb753a451b353f9094
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-01-18 11:20:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './routes/Home';
+import About from './routes/About';
+import Project from './routes/Project';
+import Contact from './routes/Contact';
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    
+    </>
+  );
+}
+
+export default App;
