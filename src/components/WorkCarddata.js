36c8262eaf8a874de74ace61d2a20d5e1df0bb36
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-01-18 16:58:29 +0530

    Create WorkCarddata.js

---
+++ src/components/WorkCarddata.js
@@ -0,0 +1,47 @@
+import pro1 from "../assets/project1.jpg"
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/password.jpg"
+import pro4 from "../assets/project4.jpg"
+import pro5 from "../assets/Quiz.jpg"
+import pro6 from "../assets/project5.jpg"
+
+const Projectcarddata=[
+    {
+        imgsrc:pro1,
+        title:"Razorpay-Clone",
+        text:"I undertook the Razorpay Clone project as a hands-on initiative to deepen my understanding of web development and showcase my proficiency in replicating complex applications.",
+        view:"https://github.com/Anmolp23/Razorpay-Clone"
+    },
+    {
+        imgsrc:pro4,
+        title:"Tourism-React",
+        text:"The Tourism  project is a dynamic web application built to showcase tourist destinations, provide information, and enhance the user experience in exploring various travel options.",
+        view:"https://github.com/Anmolp23/Tourism-React"
+    },
+    {
+        imgsrc:pro3,
+        title:"Password-Generator",
+        text:"The Password Generator offers features to facilitate a secure password creation process.Users can customize the length of their passwords.",
+        view:"https://github.com/Anmolp23/Password-Generator-in-JS"
+    },
+    {
+        imgsrc:pro2,
+        title:"Weather-App",
+        text:"The Weather  App project   showcases my skills in  front-end development,   API integration, and user interface design.Demonstrated proficiency in working with APIs, showcasing the ability to integrate external data sources seamlessly into web applications.        ",
+        view:"https://github.com/Anmolp23/Weather-App"
+    },
+    {
+        imgsrc:pro5,
+        title:"Quiz-App",
+        text:"The Quiz App, a dynamic web application designed to provide users with an engaging and interactive platform for testing their knowledge. This project was conceived with a focus on creating an intuitive user experience while incorporating a variety of features to enhance the quiz-taking process.",
+        view:"https://github.com/Anmolp23/Quiz-app-using-javascript"
+    },
+    {
+        imgsrc:pro6,
+        title:"Educational Website",
+        text:"The Educational Website project is a comprehensive online platform designed to facilitate learning and knowledge-sharing. With a focus on creating an interactive and user-friendly environment, this website aims to provide valuable educational resources, courses, and a collaborative space for learners.",
+        view:"https://github.com/Anmolp23/Projects/tree/main/Education%20website"
+    }
+];
+
+export default Projectcarddata;
