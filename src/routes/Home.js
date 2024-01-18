c0ee85d8c82f88b4e148e7f6b175c41477098291
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-01-18 17:00:59 +0530

    Create Home.js

---
+++ src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from "../components/NavBar"
+import Hero from "../components/Hero"
+import Footer from "../components/Footer"
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
