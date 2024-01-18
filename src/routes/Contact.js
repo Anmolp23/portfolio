fab2decf51a9148695ef6297b546fe857aa3b16b
Anmol parihar <121367744+Anmolp23@users.noreply.github.com>
2024-01-18 16:59:51 +0530

    Create Contact.js

---
+++ src/routes/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+import Hero2 from "../components/Hero2"
+import Form from "../components/Form"
+const Contact = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero2 heading="CONTACT" text="Let's have a chat" />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact
